8e175f8a2ba07e7d8521114c8a9dec5b338e3fd4
lc <1261406704@qq.com>
2020-09-14 17:49:04 +0800

    2.1初始化动态数组

---
+++ src/com/osozos/learn/_1_线性结构/_1_动态数组_Dynamic_Array/ArrayList.java
@@ -0,0 +1,83 @@
+package com.osozos.learn._1_线性结构._1_动态数组_Dynamic_Array;
+
+public class ArrayList<E> {
+    /**
+     * 元素的数量
+     * @return
+     */
+    public int size() {
+        return 0;
+    };
+
+    /**
+     * 是否为空
+     * @return
+     */
+    public boolean isEmpty() {
+        return true;
+    }
+
+    /**
+     * 是否包含某个元素
+     * @param element
+     * @return
+     */
+    public boolean contains(E element) {
+        return false;
+    }
+
+    /**
+     * 添加元素到最后面
+     * @param element
+     */
+    public void add(E element) {}
+
+    /**
+     * 往 index 位置添加元素
+     * @param index
+     * @param element
+     */
+    public void add(int index, E element) {}
+
+    /**
+     * 返回 index 位置对应的元素
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        return null;
+    }
+
+    /**
+     * 设置 index 位置的元素
+     * @param index
+     * @param element
+     * @return
+     */
+    public E set(int index, E element) {
+        return null;
+    }
+
+    /**
+     * 删除 index 位置对应的元素
+     * @param index
+     * @return
+     */
+    public E remove(int index) {
+        return null;
+    }
+
+    /**
+     * 查看元素的位置
+     * @param element
+     * @return
+     */
+    public int indexOf(E element) {
+        return 0;
+    }
+
+    /**
+     * 清除所有元素
+     */
+    public void clear() {};
+}
