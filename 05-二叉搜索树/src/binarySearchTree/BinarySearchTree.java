586378e6f712d939423a15644f618c1a794154d2
lc <1261406704@qq.com>
2020-10-12 10:35:24 +0800

    :art: 改善代码结构

--- 05-二叉搜索树/src/binarySearchTree/BinarySearchTree.java
+++ 05-二叉搜索树/src/binarySearchTree/BinarySearchTree.java
@@ -6,9 +6,7 @@ import com.sun.istack.internal.Nullable;
 
 import java.util.*;
 
-public class BinarySearchTree<E> implements BinaryTreeInfo {
-    private int size;
-    private Node<E> root;
+public class BinarySearchTree<E> extends BinaryTree {
     private Comparator<E> comparator;
 
     public BinarySearchTree() {
@@ -17,19 +15,6 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
 
     public BinarySearchTree(@Nullable Comparator comparator) {
         this.comparator = comparator;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public void clear() {
-        root = null;
-        size = 0;
     }
 
     public void add(@NotNull E element) {
@@ -104,42 +89,6 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         return Objects.nonNull(node(element));
     }
 
-    public int height() {
-        if (Objects.isNull(root)) {
-            return 0;
-        }
-        Queue<Node<E>> queue = new LinkedList<>();
-        queue.offer(root);
-        int height = 0;
-        int levelSize = queue.size();
-        while (!queue.isEmpty()) {
-            Node<E> node = queue.poll();
-            levelSize--;
-            if (Objects.nonNull(node.left)) {
-                queue.offer(node.left);
-            }
-            if (Objects.nonNull(node.right)) {
-                queue.offer(node.right);
-            }
-            if (levelSize == 0) {
-                levelSize = queue.size();
-                height++;
-            }
-        }
-        return height;
-    }
-
-    public int height2() {
-        return height2(root);
-    }
-
-    private int height2(Node<E> node) {
-        if (Objects.isNull(node)) {
-            return 0;
-        }
-        return 1 + Math.max(height2(node.left), height2(node.right));
-    }
-
     public void levelOrder(Visitor<E> visitor) {
         if (Objects.isNull(visitor)) {
             return;
@@ -197,127 +146,6 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         visitor.visit(node.element);
     }
 
-    public void preorderTraversal() {
-        preorderTraversal(root);
-    }
-
-    private void preorderTraversal(Node<E> node) {
-        if (Objects.isNull(node)) {
-            return;
-        }
-        System.out.print(node.element + ",");
-        preorderTraversal(node.left);
-        preorderTraversal(node.right);
-    }
-    
-    public void inorderTraversal() {
-        inorderTraversal(root);
-    }
-
-    private void inorderTraversal(Node<E> node) {
-        if (Objects.isNull(node)) {
-            return;
-        }
-        inorderTraversal(node.left);
-        System.out.print(node.element + ",");
-        inorderTraversal(node.right);
-    }
-
-    public void postorderTraversal() {
-        postorderTraversal(root);
-    }
-
-    private void postorderTraversal(Node<E> node) {
-        if (Objects.isNull(node)) {
-            return;
-        }
-        postorderTraversal(node.left);
-        postorderTraversal(node.right);
-        System.out.print(node.element + ",");
-    }
-
-    public void levelOrderTraversal() {
-        if (Objects.isNull(root)) {
-            return;
-        }
-        Queue<Node<E>> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            Node<E> node = queue.poll();
-            System.out.print(node.element + ",");
-            if (Objects.nonNull(node.left)) {
-                queue.offer(node.left);
-            }
-            if (Objects.nonNull(node.right)) {
-                queue.offer(node.right);
-            }
-        }
-    }
-
-    private Node<E> predecessor(Node<E> node) {
-        if (Objects.isNull(node)) {
-            return null;
-        }
-
-        // 前驱节点在左子树中
-        Node<E> p = node.left;
-        if (Objects.nonNull(p)) {
-            while (Objects.nonNull(p.right)) {
-                p = p.right;
-            }
-            return p;
-        }
-
-        // 从父节点、祖父节点中寻找前驱节点
-        while (Objects.nonNull(node.parent) && node == node.parent.left) {
-            node = node.parent;
-        }
-
-        return node.parent;
-    }
-
-    private Node<E> successor(Node<E> node) {
-        if (Objects.isNull(node)) {
-            return null;
-        }
-
-        // 后继节点在右子树中
-        Node<E> p = node.right;
-        if (Objects.nonNull(p)) {
-            while (Objects.nonNull(p.left)) {
-                p = p.left;
-            }
-            return p;
-        }
-
-        // 从父节点、祖父节点中寻找前驱节点
-        while (Objects.nonNull(node.parent) && node == node.parent.right) {
-            node = node.parent;
-        }
-
-        return node.parent;
-    }
-
-    @Override
-    public Object root() {
-        return root;
-    }
-
-    @Override
-    public Object left(Object node) {
-        return ((Node<E>)node).left;
-    }
-
-    @Override
-    public Object right(Object node) {
-        return ((Node<E>)node).right;
-    }
-
-    @Override
-    public Object string(Object node) {
-        return ((Node<E>)node).element;
-    }
-
     private Node<E> node(E element) {
         Node<E> node = root;
         int cmp;
@@ -345,25 +173,5 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
 
     public static interface Visitor<E> {
         void visit(E element);
-    }
-
-    private static final class Node<E> {
-        E element;
-        Node<E> left;
-        Node<E> right;
-        Node<E> parent;
-
-        public Node(E element, Node<E> parent) {
-            this.element = element;
-            this.parent = parent;
-        }
-
-        public boolean isLeaf() {
-            return Objects.isNull(left)&& Objects.isNull(right);
-        }
-
-        public boolean hasTwoChildren() {
-            return Objects.nonNull(left) && Objects.nonNull(right);
-        }
     }
 }
---
+++ 05-二叉搜索树/src/binarySearchTree/BinaryTree.java
@@ -0,0 +1,148 @@
+package binarySearchTree;
+
+import binarySearchTree.printer.BinaryTreeInfo;
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
+public class BinaryTree<E> implements BinaryTreeInfo {
+    protected int size;
+    protected Node<E> root;
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        root = null;
+        size = 0;
+    }
+
+    public int height() {
+        if (Objects.isNull(root)) {
+            return 0;
+        }
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.offer(root);
+        int height = 0;
+        int levelSize = queue.size();
+        while (!queue.isEmpty()) {
+            Node<E> node = queue.poll();
+            levelSize--;
+            if (Objects.nonNull(node.left)) {
+                queue.offer(node.left);
+            }
+            if (Objects.nonNull(node.right)) {
+                queue.offer(node.right);
+            }
+            if (levelSize == 0) {
+                levelSize = queue.size();
+                height++;
+            }
+        }
+        return height;
+    }
+
+    public int height2() {
+        return height2(root);
+    }
+
+    private int height2(Node<E> node) {
+        if (Objects.isNull(node)) {
+            return 0;
+        }
+        return 1 + Math.max(height2(node.left), height2(node.right));
+    }
+
+    protected Node<E> predecessor(Node<E> node) {
+        if (Objects.isNull(node)) {
+            return null;
+        }
+
+        // 前驱节点在左子树中
+        Node<E> p = node.left;
+        if (Objects.nonNull(p)) {
+            while (Objects.nonNull(p.right)) {
+                p = p.right;
+            }
+            return p;
+        }
+
+        // 从父节点、祖父节点中寻找前驱节点
+        while (Objects.nonNull(node.parent) && node == node.parent.left) {
+            node = node.parent;
+        }
+
+        return node.parent;
+    }
+
+    protected Node<E> successor(Node<E> node) {
+        if (Objects.isNull(node)) {
+            return null;
+        }
+
+        // 后继节点在右子树中
+        Node<E> p = node.right;
+        if (Objects.nonNull(p)) {
+            while (Objects.nonNull(p.left)) {
+                p = p.left;
+            }
+            return p;
+        }
+
+        // 从父节点、祖父节点中寻找前驱节点
+        while (Objects.nonNull(node.parent) && node == node.parent.right) {
+            node = node.parent;
+        }
+
+        return node.parent;
+    }
+
+    @Override
+    public Object root() {
+        return root;
+    }
+
+    @Override
+    public Object left(Object node) {
+        return ((Node<E>)node).left;
+    }
+
+    @Override
+    public Object right(Object node) {
+        return ((Node<E>)node).right;
+    }
+
+    @Override
+    public Object string(Object node) {
+        return ((Node<E>)node).element;
+    }
+
+    protected static final class Node<E> {
+        E element;
+        Node<E> left;
+        Node<E> right;
+        Node<E> parent;
+
+        public Node(E element, Node<E> parent) {
+            this.element = element;
+            this.parent = parent;
+        }
+
+        public boolean isLeaf() {
+            return Objects.isNull(left)&& Objects.isNull(right);
+        }
+
+        public boolean hasTwoChildren() {
+            return Objects.nonNull(left) && Objects.nonNull(right);
+        }
+    }
+}
