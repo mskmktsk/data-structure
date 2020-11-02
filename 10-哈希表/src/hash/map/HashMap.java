b57a56fd8e537d0289e48acff323b53f106ffc80
So-Studio <appledeveloper@so.studio>
2020-11-02 17:30:45 +0800

    :construction: 编写 put 方法中

--- 10-哈希表/src/hash/map/HashMap.java
+++ 10-哈希表/src/hash/map/HashMap.java
@@ -35,6 +35,45 @@ public class HashMap<K, V> implements Map<K, V> {
 
     @Override
     public V put(K key, V value) {
+        int index = index(key);
+        // 取出 index 索引上的红黑树根节点
+        Node<K, V> root = table[index];
+        if (Objects.isNull(root)) {
+            root = new Node<>(key, value, null);
+            table[index] = root;
+            size++;
+            afterPut(root);
+            return null;
+        }
+
+        // 添加新的节点到红黑树上
+
+        Node<K, V> parent = root;
+        Node<K, V> node = root;
+        int cmp = 0;
+        do {
+            cmp = compareTo(key, node.key);
+            parent = node;
+            if (cmp > 0) {
+                node = node.right;
+            } else if (cmp < 0) {
+                node = node.left;
+            } else {
+                node.key = key;
+                V oldValue = node.value;
+                node.value = value;
+                return oldValue;
+            }
+        } while (node != null);
+        Node<K, V> cNode = new Node<>(key, value, parent);
+        if (cmp > 0) {
+            parent.right = cNode;
+        } else {
+            parent.left = cNode;
+        }
+        size++;
+        // 新添加之后的处理
+        afterPut(cNode);
         return null;
     }
 
@@ -62,6 +101,125 @@ public class HashMap<K, V> implements Map<K, V> {
     public void traversal(Visitor<K, V> visitor) {
 
     }
+
+    private int index(K key) {
+        if (Objects.isNull(key)) return 0;
+        int hash = key.hashCode();
+        hash = hash ^ (hash >>> 16);
+        return hash & (table.length - 1);
+    }
+
+    private int compareTo(K key1, K key2) {
+        return 0;
+    }
+
+    private void afterPut(Node<K, V> node) {
+
+        Node<K, V> parent = node.parent;
+
+        if (Objects.isNull(parent)) {
+            black(node);
+            return;
+        }
+
+        if (isBlack(parent)) {
+            return;
+        }
+
+        Node<K, V> uncle = parent.sibling();
+        Node<K, V> grand = red(parent.parent);
+
+        if (isRed(uncle)) {
+            black(parent);
+            black(uncle);
+            afterPut(grand);
+            return;
+        }
+
+        if (parent.isLeftChild()) { // L
+            if (node.isLeftChild()) { // LL
+                black(parent);
+            } else { // LR
+                black(node);
+                rotateLeft(parent);
+            }
+            rotateRight(grand);
+        } else { // R
+            if (node.isRightChild()) { // RR
+                black(parent);
+            } else {
+                black(node);
+                rotateRight(parent);
+            }
+            rotateLeft(grand);
+        }
+    }
+
+    private void rotateLeft(Node<K, V> grand) {
+        Node<K, V> parent = grand.right;
+        Node<K, V> child = parent.left;
+        grand.right = child;
+        parent.left = grand;
+
+        afterRotate(child, parent, grand);
+    }
+
+    private void rotateRight(Node<K, V> grand) {
+        Node<K, V> parent = grand.left;
+        Node<K, V> child = parent.right;
+        grand.left = child;
+        parent.right = grand;
+
+        afterRotate(child, parent, grand);
+    }
+
+    private void afterRotate(Node<K, V> child, Node<K, V> parent, Node<K, V> grand) {
+        // 让 parent 成为子树的根节点
+        parent.parent = grand.parent;
+        if (grand.isLeftChild()) {
+            grand.parent.left = parent;
+        } else if (grand.isRightChild()) {
+            grand.parent.right = parent;
+        } else { // node 是 root 节点
+            root = parent;
+        }
+
+        // 更新 child 的 parent
+        if (Objects.nonNull(child)) {
+            child.parent = grand;
+        }
+
+        // 更新 grand 的 parent
+        grand.parent = parent;
+    }
+
+    private Node<K, V> color(Node<K, V> node, boolean color) {
+        if (Objects.nonNull(node)) {
+            node.color = color;
+        }
+        return node;
+    }
+
+    private Node<K, V> black(Node<K, V> node) {
+        return color(node, BLACK);
+    }
+
+    private Node<K, V> red(Node<K, V> node) {
+        return color(node, RED);
+    }
+
+    private boolean colorOf(Node<K, V> node) {
+        return Objects.isNull(node) ? BLACK : node.color;
+    }
+
+    private boolean isBlack(Node<K, V> node) {
+        return colorOf(node) == BLACK;
+    }
+
+    private boolean isRed(Node<K, V> node) {
+        return colorOf(node) == RED;
+    }
+
 
     private static class Node<K, V> {
         K key;
